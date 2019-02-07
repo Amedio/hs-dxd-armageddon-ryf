b781fa7bdcb3750b9d22220e644d5dadfa94cc5b
Amedio <rubenralc@gmail.com>
2019-02-07 13:18:23 +0100

    roll command now allows to add an effect roll before it

--- bot.py
+++ bot.py
@@ -27,7 +27,7 @@ async def on_ready():
     print('------')
 
 @bot.command()
-async def roll(ctx, bonus:int=0, difficulty:int=0, dice:str='n'):
+async def roll(ctx, bonus:int=0, difficulty:int=0, dice:str='n', e_dice_amount:int=0, e_bonus:int=0):
     dice_index = 1
     if dice == 'down' or dice == 'd':
         dice_index = 0
@@ -68,8 +68,13 @@ async def roll(ctx, bonus:int=0, difficulty:int=0, dice:str='n'):
                 rich.add_field(name="resultado", value="FALLO", inline=True)
             if total - difficulty >= 10:
                 rich.add_field(name="crítico", value="CRÍTICO", inline=True)
+                critical_times = total - difficulty / 10
+                e_dice_amount = e_dice_amount + critical_times
 
     await ctx.send(embed=rich)
+
+    if total >= difficulty and e_dice_amount > 0:
+        effect(ctx, e_dice_amount, e_bonus)
 
 @bot.command()
 async def effect(ctx, dice_amount:int, bonus:int=0):
